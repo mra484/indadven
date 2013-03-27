a3fb5a92f0a4ed0daaa44b06ec8578261d6f64e0
Mark Andrews <mandrews18@knights.ucf.edu>
2013-03-27 19:39:10 -0400

    First

---
+++ Assignment 4/src/Board.java
@@ -0,0 +1,27 @@
+
+public class Board {
+
+	public Board(int rows, int cols){
+		
+		
+	}
+	
+	public Cave getCave(int r, int c){
+		
+		
+		return Cave;
+	}
+	
+	public boolean ok(int r, int c){
+		
+		
+		return true;
+	}
+	
+	public Cave getUnoccupiedOpenLocation(){
+		
+		return Cave;
+	}
+	
+	
+}
