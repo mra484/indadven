aa27c0bc5920e41cce115807eb18ddcbe6b84357
Mark Andrews <mandrews18@knights.ucf.edu>
2013-03-27 20:03:00 -0400

    2

--- Assignment 4/src/Adventurer.java
+++ Assignment 4/src/Adventurer.java
@@ -1,4 +1,34 @@
 
-public class Adventurer {
-
+public class Adventurer extends Character{
+	 public Adventurer(Cave intiLoc){
+		 super(initLoc);
+		 
+		 
+	 }
+	 
+	 public boolean modifyCave(Cave loc){
+		 
+		 
+		 return false;
+	 }
+	 
+	 
+	 public String describeModification(){
+		 
+		 
+		 return "PH";
+	 }
+	 
+	 public boolean move(Cave to){
+		 
+		 
+		 return false;
+	 }
+	 
+	 
+	 public String getName(){
+		 
+		 
+		 return "PH";
+	 }
 }
--- Assignment 4/src/Cave.java
+++ Assignment 4/src/Cave.java
@@ -1,4 +1,91 @@
 
 public class Cave {
-
+	
+	public Cave(int r, int c){
+		
+	}
+	
+	public int getRow(){
+		
+		return ;
+	}
+	
+	public int getCol(){
+		
+		return ;
+	}
+	
+	public void setOccupied(boolean set){
+		
+		
+	}
+	
+	public boolean isOccupied(){
+		
+		
+		return false;
+	}
+	
+	public void setMarked(boolean set){
+		
+		
+		
+	}
+	
+	public boolean isMarked(){
+		
+		
+		return false;
+	}
+	
+	
+	public void makeOpen(){
+		
+		
+	}
+	
+	public boolean isOpen(){
+		
+		
+		return false;
+	}
+	
+	public void makeBlocked(){
+		
+		
+	}
+	
+	public boolean isBlocked(){
+		
+		
+		return false;
+	}
+	
+	
+	public void makePit(){
+		
+		
+	}
+	
+	
+	public boolean isPit(){
+		
+		
+		return false;
+	}
+	
+	
+	public void makeTeleport(){
+		
+		
+	}
+	
+	
+	public boolean isTeleport(){
+		
+		
+		
+		return false;
+	}
+	
 }
