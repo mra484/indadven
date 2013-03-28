96fa399e2b6e95e8a3f6b690ce7768eff892304c
Mark Andrews <mandrews18@knights.ucf.edu>
2013-03-27 20:19:18 -0400

    4

--- Assignment 4/src/Miner.java
+++ Assignment 4/src/Miner.java
@@ -1,4 +1,32 @@
 
-public class Miner {
+public class Miner extends Character {
+	
+	public Miner(Cave initLoc){
+		super(initLoc);
+		
+	}
+	
+	public String describeModification(){
+		
+		
+		return "PH";
+	}
+	
+	public boolean move(Cave to){
+		
+		
+		return false;
+	}
+	
+	
+	public String getName(){
+		
+		
+		
+		return "PH";
+	}
+	
 
+	
+	
 }
